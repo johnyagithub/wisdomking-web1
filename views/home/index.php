d8620a14502a329640118f13771f252b35dd16bd
johnyagithub <jonya1530@gmail.com>
2024-10-22 15:17:04 +0700

    save

--- views/home/index.php
+++ views/home/index.php
@@ -20,18 +20,58 @@
 		<section class="box-banner">
 			<div class="container">
 				<div class="owl-carousel">
-	
+
 					<div class="item">
 						<img class="img-banner" src="../../public/images/banner.jpg">
 						<div class="box-text p-5">
 							<div class="b-text">
-								<h1 class="text-white mb-4">สำนักงานพิพิธภัณฑ์เกษตรเฉลิมพระเกียรติพระบาทสมเด็จพระเจ้าอยู่หัว(องค์การมหาชน)</h1>
-								<p class="text-white mb-4">"เป็นพิพิธภัณฑ์ชั้นนำแห่งการเรียนรู้พระเกียรติคุณและพระอัจฉริยภาพของกษัตริย์เกษตรและเป็นศูนย์กลางการเรียนรู้หลักปรัชญาของเศรษฐกิจ พอเพียง นวัตกรรมด้านการเกษตรของประเทศ"</p>
+								<h1 class="text-white mb-4">
+									สำนักงานพิพิธภัณฑ์เกษตรเฉลิมพระเกียรติพระบาทสมเด็จพระเจ้าอยู่หัว(องค์การมหาชน)</h1>
+								<p class="text-white mb-4">
+									"เป็นพิพิธภัณฑ์ชั้นนำแห่งการเรียนรู้พระเกียรติคุณและพระอัจฉริยภาพของกษัตริย์เกษตรและเป็นศูนย์กลางการเรียนรู้หลักปรัชญาของเศรษฐกิจ
+									พอเพียง นวัตกรรมด้านการเกษตรของประเทศ"</p>
 								<a class="btn btn-light px-4 rounded-pill" href="#">อ่านต่อ</a>
 							</div>
 						</div>
 					</div>
-	
+
+				</div>
+			</div>
+		</section>
+		<section class="py-5">
+			<div class="container">
+				<h5 class="head text-center text-primary font-weight-lighter d-table mx-auto">เกี่ยวกับเรา</h5>
+				<p class="h4 text-center text-secondary font-weight-lighter">
+					กระทรวงเกษตรและสหกรณ์ <b class="text-primary font-weight-lighter">ได้จัดตั้งพิพิธภัณฑ์การเกษตรเฉลิม<br>
+						พระเกียรติพระบาทสมเด็จพระเจ้าอยู่หัวโดยมีวัตถุประสงค์เพื่อเฉลิม พระเกียรติ</b><br>
+					พระบาทสมเด็จพระบรม ชนกาธิเบศรมหาภูมิพลอดุลยเดชมหาราช บรมนาถบพิตร<br>
+					เนื่องในมหามงคลสมัยที่ทรง...
+				</p>
+				<br>
+				<a class="btn btn-style px-4 d-table mx-auto" href="#" role="button">อ่านต่อ</a>
+			</div>
+		</section>
+
+		<section class="box-museum">
+			<div class="container py-5">
+				<h3 class="text-white head text-center mb-5">พิพิธภัณฑ์<br>แห่งการเรียนรู้เกษตรเศรษฐกิจพอเพียง</h3>
+				<div class="row">
+					<div class="col-md-6 col-lg-3">
+						<img src="../../public/images/museum/img1.jpg" alt="" class="--img">
+						<p>เผยแพร่ พระเกียรติคุณ และ พระอัจฉริยภาพด้านการเกษตร ของพระมหากษัตริย์ไทย และพระบรม วงศานุวงศ์</p>
+					</div>
+					<div class="col-md-6 col-lg-3">
+						<img src="../../public/images/museum/img2.jpg" alt="" class="--img">
+						<p>สืบสาน รักษา ต่อยอด หลักปรัชญา ของเศรษฐกิจพอเพียงด้านการเกษตร</p>
+					</div>
+					<div class="col-md-6 col-lg-3">
+						<img src="../../public/images/museum/img3.jpg" alt="" class="--img">
+						<p>บริหารจัดการองค์กรให้เป็นพิพิธภัณฑ์ แห่งการเรียนรู้หลักปรัชญาของเศรษฐกิจพอเพียงของประเทศ</p>
+					</div>
+					<div class="col-md-6 col-lg-3">
+						<img src="../../public/images/museum/img4.jpg" alt="" class="--img">
+						<p>ส่งเสริม สนับสนุน เครือข่ายและ ภาคีความ ร่วมมือในการขับเคลื่อนหลัก ปรัชญาของ เศรษฐกิจพอเพียง ด้านการเกษตร</p>
+					</div>
 				</div>
 			</div>
 		</section>
