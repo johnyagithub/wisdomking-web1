5c2e9b21fe39ddf970a3e8681c95740f5a4afb02
John <bitbucket@duoflash.net>
2024-12-02 20:19:13 +0700

    2

--- views/complaint/receive-complaints-corrupt3.php
+++ views/complaint/receive-complaints-corrupt3.php
@@ -6,6 +6,8 @@
 	<?php include('../layouts/inc-meta.php'); ?>
 	<!-- select2 -->
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css" rel="stylesheet" />
+	<!-- FileUp -->
+	<link href="../../plugins/jQuery-AJAX-File-Uploader-FileUp/src/fileup.css" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -40,14 +42,14 @@
 						industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
 				</div>
 				<div class="row">
-					<div class="col-lg-5 col-xl-4 --sumMenu">
+					<div class="col-lg-5 col-xl-4 mb-3 --sumMenu">
 
 						<div class="sticky-top" style="top: 113px;">
 							<a href="javascript:void(0)" data-toggle="dropdown" aria-expanded="false">
-								ช่องทางแจ้งเรื่องร้องเรียน
+								รับเรื่องร้องเรียน ทุจริต และประพฤติมิชอบ
 							</a>
 							<div class="sumMenu-profile dropdown-menu">
-								<a class="dropdown-item active" href="../complaint">
+								<a class="dropdown-item" href="../complaint">
 									ช่องทางแจ้งเรื่องร้องเรียน
 								</a>
 								<div class="dropdown">
@@ -69,10 +71,10 @@
 									</div>
 								</div>
 								<div class="dropdown">
-									<a class="dropdown-item" href="javascript:void(0)" data-toggle="dropdown" aria-expanded="false">
+									<a class="dropdown-item active" href="javascript:void(0)" data-toggle="dropdown" aria-expanded="false">
 										รับเรื่องร้องเรียน ทุจริต และประพฤติมิชอบ
 									</a>
-									<div class="dropdown-menu">
+									<div class="dropdown-menu show">
 										<a class="dropdown-item" href="../complaint/receive-complaints-corrupt1.php">
 											เรื่องทั่วไป
 										</a>
@@ -81,7 +83,7 @@
 											menu 2
 										</a>
 										<div class="dropdown-divider"></div>
-										<a class="dropdown-item" href="../complaint/receive-complaints-corrupt3.php">
+										<a class="dropdown-item active" href="../complaint/receive-complaints-corrupt3.php">
 											menu 3
 										</a>
 									</div>
@@ -91,8 +93,7 @@
 
 					</div>
 					<div class="col mt-4 mt-md-0">
-						<img
-							src="https://www.wisdomking.or.th/files/media_manager/12075f8459499dd19b4bc0eed7ccbf39/523-0.57635200%201730880042-froalaeditor.png" alt="" class="d-block mx-auto rounded w-100">
+						<img src="https://placehold.co/500x1000" alt="" class="d-block mx-auto rounded w-100">
 						<div class="mx-auto" style="max-width: 800px;">
 							<div class="list-download">
 								<div class="list">
@@ -120,6 +121,27 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
 	<script>
 		$(".templatingSelect2").select2();
+	</script>
+	<!-- FileUp -->
+	<script src="../../plugins/jQuery-AJAX-File-Uploader-FileUp/src/fileup.js"></script>
+	<script>
+		$.fileup({
+			url: '#',
+			inputID: 'upload',
+			queueID: 'upload-queue',
+			autostart: true,
+			// onSelect: function (file) {
+			// },
+			// onRemove: function (file, total) {
+			// },
+			onSuccess: function (response, file_number, file) {
+				$('#file-upload-' + file_number).addClass('--success');
+			},
+			onError: function (event, file, file_number) {
+				$('#file-upload-' + file_number).addClass('--error');
+				$('#file-upload-' + file_number).find('.result').text('This document is not supported, please delete and upload another file.');
+			}
+		});
 	</script>
 
 </body>
