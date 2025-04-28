367b761c92b602a8a780af7d1f7beae8e9acff2d
johnyagithub <jonya1530@gmail.com>
2025-04-28 13:22:21 +0700

    1

--- views/complaint/inc-receive-menu.php
+++ views/complaint/inc-receive-menu.php
@@ -57,7 +57,7 @@
   <div class="sumMenu-profile dropdown-menu">
     <a class="dropdown-item" href="../complaint">ระบบแจ้งเรื่องร้องเรียน</a>
     <div class="dropdown">
-      <a class="dropdown-item" href="javascript:void(0)" data-toggle="dropdown" aria-expanded="false">การแจ้งเรื่องร้องเรียนทั่วไป</a>
+      <a class="dropdown-item" href="javascript:void(0)" onclick="event.stopPropagation(); $(this).parent().siblings().find('.dropdown-menu').hide(); $(this).next('.dropdown-menu').toggle();" data-toggle="dropdown" aria-expanded="false">การแจ้งเรื่องร้องเรียนทั่วไป</a>
       <div class="dropdown-menu">
         <a class="dropdown-item" href="../complaint/receive-complaints1.php">การแจ้งเรื่องร้องเรียนทั่วไป</a>
         <a class="dropdown-item" href="../complaint/receive-complaints2.php">คู่มือการปฏิบัติงาน เรื่องร้องเรียน</a>
@@ -67,7 +67,7 @@
       </div>
     </div>
     <div class="dropdown">
-      <a class="dropdown-item" href="javascript:void(0)" data-toggle="dropdown" aria-expanded="false">การแจ้งเรื่องร้องเรียนการทุจริต</a>
+      <a class="dropdown-item" href="javascript:void(0)" onclick="event.stopPropagation(); $(this).parent().siblings().find('.dropdown-menu').hide(); $(this).next('.dropdown-menu').toggle();" data-toggle="dropdown" aria-expanded="false">การแจ้งเรื่องร้องเรียนการทุจริต</a>
       <div class="dropdown-menu">
         <a class="dropdown-item" href="../complaint/receive-complaints-corrupt1.php">การแจ้งเรื่องร้องเรียนการทุจริต</a>
         <a class="dropdown-item" href="../complaint/receive-complaints-corrupt2.php">คู่มือแนวทางปฏิบัติ</a>
